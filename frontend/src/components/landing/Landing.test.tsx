7fe795950024cd13e3eb237d613f28278d1c7d69
Máté Szendrő <szemate@posteo.net>
2022-12-11 18:33:55 +0100

    Add React Router

--- frontend/src/components/landing/Landing.test.tsx
+++ frontend/src/components/landing/Landing.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Landing from './Landing';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Landing />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Landing.scss';
 
-function App() {
+export default function Landing() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Landing">
+      <header className="Landing-header">
+        <img src={logo} className="Landing-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Landing-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
