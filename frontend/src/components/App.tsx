4014d33173f61fd60069c812ab3452a5df489971
andrisnemeth <n.andris939@gmail.com>
2022-12-13 17:04:13 +0100

    Update App.tsx with notImplementedPage route.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from './landing/Landing';
+import NotImplementedPage from './notimplentedpage/NotImplentedPage';
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<NotImplementedPage />} />
       </Routes>
     </BrowserRouter>
   );
