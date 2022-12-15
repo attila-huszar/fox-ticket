b2a2c7184991d665880e18b497dd33261caf420d
hagles <balazs.tamas.86@gmail.com>
2022-12-15 18:46:19 +0100

    add CSS modification to Cart and Shop

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Landing.scss";
-
+import Shop from "../shop/Shop"
 
 export default function Landing() {
   return (
     <div className="Landing">
       <div className="menu">
-       
-      
-      
+    
     </div>
     </div>
   );
