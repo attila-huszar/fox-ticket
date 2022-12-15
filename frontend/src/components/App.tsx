7fbfd2c978f8a884d265d2998f03bc014b3bcc31
hagles <balazs.tamas.86@gmail.com>
2022-12-15 11:52:39 +0100

    remove Registration ticket as it parto fanother branch and was mistakenly was added to menu branch

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./landing/Landing";
-import Shop from "./shop/Shop";
-import Cart from "./cart/Cart";
-import Registration from "./registration/Registration";
 
 export default function App() {
   return (
--- frontend/src/components/registration/Registration.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Registration from './Registration';
-
-test('renders learn react link', () => {
-  render(<Registration />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
