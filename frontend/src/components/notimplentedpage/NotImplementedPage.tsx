09cec52e4c6c361b28b44ab58d5fbe08c4ab22fa
andrisnemeth <n.andris939@gmail.com>
2022-12-15 13:31:54 +0100

    Update tsx with image and added files: image and scss.

--- frontend/src/components/notimplentedpage/NotImplementedPage.tsx
+++ frontend/src/components/notimplentedpage/NotImplementedPage.tsx
@@ -1,7 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import "./NotImplementedPage.scss"
+import logo from './fox.png'
+import { useRef } from 'react';
 
 export default function NotImplementedPage() {
+  const refLogo = useRef<HTMLImageElement>(null);
 
   const navigate = useNavigate();
 
@@ -14,6 +17,7 @@ export default function NotImplementedPage() {
     <div className="NotImplementedPage">
       <div className='notImplemented-body'>
         <h1>This page is not implemented yet!</h1>
+        <img src={logo} ref={refLogo} id='logo'alt='logo'/>
         <button type='submit' onClick={routeChange} id='goHomeButton' className="button">Go Home</button>
       </div>
     </div>
