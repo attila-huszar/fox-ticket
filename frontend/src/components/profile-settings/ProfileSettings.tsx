bd3b26f8fe77230c6610c72cb92d2522f6c15ddc
Gergely Valkai <gergely.valkai03@gmail.com>
2022-12-16 10:38:37 +0100

    add click handler

--- frontend/src/components/profile-settings/ProfileSettings.tsx
+++ frontend/src/components/profile-settings/ProfileSettings.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./ProfileSettings.css";
 
 export default function ProfileSettings() {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleClick = () => {
+    setErrorMessage("A field is required to be changed.");
+  };
+
   return (
     <div className="settings">
       <h1>Profile</h1>
@@ -22,9 +29,6 @@ export default function ProfileSettings() {
 
           <div className="input-container">
             <input className="input-field" type="password" id="id_password" name="oldPassword" autoComplete="current-password" minLength={6} required placeholder=" " />
-            <div className="eye" data-tooltip="Show/Hide Password">
-              <i className="fa-regular fa-fw fa-eye" id="eyeId"></i>
-            </div>
             <span className="floating-label">Old Password</span>
           </div>
           <a href="./forgot" id="forgot-pass" data-tooltip="Send email reminder" data-tooltip-location="bottom">
@@ -34,25 +38,20 @@ export default function ProfileSettings() {
           <div className="sep"></div>
 
           <div className="input-container">
-            <input className="input-field" type="password" id="id_password" name="newPassword" minLength={6} required placeholder=" " />
-            <div className="eye" data-tooltip="Show/Hide Password">
-              <i className="fa-regular fa-fw fa-eye" id="eyeId"></i>
-            </div>
+            <input className="input-field" type="password" id="id_newPassword" name="newPassword" minLength={6} required placeholder=" " />
             <span className="floating-label">New Password</span>
           </div>
 
           <div className="sep"></div>
 
           <div className="input-container">
-            <input className="input-field" type="password" id="id_password" name="newPasswordAgian" minLength={6} required placeholder=" " />
-            <div className="eye" data-tooltip="Show/Hide Password">
-              <i className="fa-regular fa-fw fa-eye" id="eyeId"></i>
-            </div>
+            <input className="input-field" type="password" id="id_newPasswordAgain" name="newPasswordAgian" minLength={6} required placeholder=" " />
             <span className="floating-label">New Password Again</span>
           </div>
 
+          {errorMessage && <div className="error">{errorMessage}</div>}
           <div className="buttons">
-            <button id="submit-btn" type="button">
+            <button id="submit-btn" type="button" onClick={handleClick}>
               Submit
             </button>
           </div>
