e9ea7e2a39e58e343c8632b9cdcc120d5e4bcd5b
Attila Huszár <attila.huszar@outlook.com>
2022-12-17 19:17:58 +0100

    Add Dark mode switch

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,23 @@
+import { createTheme, NextUIProvider, Switch } from "@nextui-org/react";
+import { useDarkMode } from "usehooks-ts";
+import { TbSun, TbMoon } from "react-icons/tb";
+import "./DarkMode.css";
+
+const lightTheme = createTheme({
+  type: "light",
+});
+const darkTheme = createTheme({
+  type: "dark",
+});
+
+export default function DarkMode() {
+  const { isDarkMode, toggle } = useDarkMode();
+
+  return (
+    <NextUIProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <div>
+        <Switch checked={isDarkMode} onChange={toggle} id="darkModeToggle" iconOn={<TbMoon />} iconOff={<TbSun />} size="lg" color="secondary" shadow />
+      </div>
+    </NextUIProvider>
+  );
+}
