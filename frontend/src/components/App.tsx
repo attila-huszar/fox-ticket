b68c6fa63699e4c9e68c2111a43d9ab395cad9fe
hagles <balazs.tamas.86@gmail.com>
2023-01-02 15:42:20 +0100

    add Card and Shop based on NextUI

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./landing/Landing";
-// import NotImplementedPage from './notimplentedpage/NotImplementedPage';
+import Cart from "./cart/Cart";
+import Shop from "./shop/Shop";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* <Route path="*" element={<NotImplementedPage />} /> */}
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/shop" element={<Shop/>} />
       </Routes>
     </BrowserRouter>
   );
