10bf59b69e6972dd8bf2d71c8e6ebe8e834db19e
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-03 09:57:31 +0100

    change profilesettings code to nextUI

--- frontend/src/components/profile-settings/ProfileSettings.tsx
+++ frontend/src/components/profile-settings/ProfileSettings.tsx
@@ -1,5 +1,5 @@
+import { Button, Input, Spacer, Grid, Text, Col, Row } from "@nextui-org/react";
 import { useState } from "react";
-import "./ProfileSettings.css";
 
 export default function ProfileSettings() {
   const [errorMessage, setErrorMessage] = useState("");
@@ -7,56 +7,24 @@ export default function ProfileSettings() {
   const handleClick = () => {
     setErrorMessage("A field is required to be changed.");
   };
-
   return (
-    <div className="settings">
-      <h1>Profile</h1>
-      <div className="form">
-        <form id="settings" name="settings">
-          <div className="input-container">
-            <input className="input-field" type="text" id="id_name" name="name" minLength={5} required placeholder=" " />
-            <span className="floating-label">Name</span>
-          </div>
-
-          <div className="sep"></div>
-
-          <div className="input-container">
-            <input className="input-field" type="email" id="id_email" name="email" required placeholder=" " />
-            <span className="floating-label">Email Address</span>
-          </div>
-
-          <div className="sep"></div>
-
-          <div className="input-container">
-            <input className="input-field" type="password" id="id_password" name="oldPassword" autoComplete="current-password" minLength={6} required placeholder=" " />
-            <span className="floating-label">Old Password</span>
-          </div>
-          <a href="./forgot" id="forgot-pass" data-tooltip="Send email reminder" data-tooltip-location="bottom">
-            Forgot Password?
-          </a>
-
-          <div className="sep"></div>
-
-          <div className="input-container">
-            <input className="input-field" type="password" id="id_newPassword" name="newPassword" minLength={6} required placeholder=" " />
-            <span className="floating-label">New Password</span>
-          </div>
-
-          <div className="sep"></div>
-
-          <div className="input-container">
-            <input className="input-field" type="password" id="id_newPasswordAgain" name="newPasswordAgian" minLength={6} required placeholder=" " />
-            <span className="floating-label">New Password Again</span>
-          </div>
-
-          {errorMessage && <div className="error">{errorMessage}</div>}
-          <div className="buttons">
-            <button id="submit-btn" type="button" onClick={handleClick}>
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Grid.Container justify="center" alignItems="center" alignContent="center">
+      <Text h1>My Settings</Text>
+      <Col css={{ textAlign: "center" }}>
+        <Input width="214px" labelPlaceholder="Username" />
+        <Spacer y={1.6} />
+        <Input width="214px" labelPlaceholder="Email" />
+        <Spacer y={1.6} />
+        <Input.Password width="214px" labelPlaceholder="Old Password" />
+        <Spacer y={1.6} />
+        <Input.Password width="214px" labelPlaceholder="New Password" />
+        <Spacer y={1.6} />
+        <Input.Password width="214px" labelPlaceholder="New Password Again" />
+        <Text css={{ color: "red" }}>{errorMessage}</Text>
+      </Col>
+      <Button shadow size="lg" auto color="gradient" rounded id="submit" onPress={handleClick}>
+        Submit
+      </Button>
+    </Grid.Container>
   );
 }
