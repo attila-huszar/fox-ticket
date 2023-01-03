3be3442c8468fecf12eada5023ee930ead072b2e
Attila Huszár <attila.huszar@outlook.com>
2023-01-03 10:57:50 +0100

    Add User model, Upd Navbar, dependencies

--- src/components/NotImplementedPage.tsx
+++ src/components/NotImplementedPage.tsx
@@ -15,7 +15,7 @@ export default function NotImplementedPage() {
     <div className="NotImplementedPage">
       <h1>This page is not implemented yet</h1>
       <img src={logo} id="logo" alt="logo" />
-      <Button shadow size="lg" icon={<TbSmartHome />} auto color="gradient" rounded onPress={routeChange} id="homeBtn">
+      <Button css={"margin: auto"} shadow size="lg" icon={<TbSmartHome />} auto color="gradient" rounded onPress={routeChange} id="homeBtn">
         Home
       </Button>
     </div>
