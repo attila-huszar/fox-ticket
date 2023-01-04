75c08be43ed73385aa036e640391ee7a57daa6c7
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-04 14:57:57 +0100

    add productService

---
+++ backend/src/services/productService.ts
@@ -0,0 +1,36 @@
+import { NotFoundError, ParameterError } from '../errors';
+import * as productRepo from '../repositories/productRepo';
+import {
+  NewProductRequest,
+  NewProductResponse,
+} from '../interfaces/newProduct';
+
+export async function addNewProduct(
+  newProduct: NewProductRequest
+): Promise<NewProductResponse> {
+  if (!newProduct) {
+    throw new ParameterError('Invalid product');
+  }
+  const product = await productRepo.createProduct(newProduct);
+
+  if (product) {
+    return { newProduct: product };
+  } else {
+    throw new NotFoundError();
+  }
+}
+
+export async function getProductById(productId: number) {
+  console.log(productId);
+  if (productId < 0 || !Number.isInteger(productId)) {
+    throw new ParameterError('Invalid productId');
+  }
+
+  const product = await productRepo.getProductById(productId);
+
+  if (product) {
+    return { product };
+  } else {
+    throw new NotFoundError();
+  }
+}
