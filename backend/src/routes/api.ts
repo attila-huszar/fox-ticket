476ead2893d930c66f140b142cee4cb869f0f1d2
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-04 14:57:44 +0100

    add /admin/products endpoints

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import status from 'http-status';
 import apiErrorHandler from '../middlewares/apiErrorHandler';
 import * as helloController from '../controllers/helloController';
+import * as productController from '../controllers/productController';
 import { HttpError } from '../errors';
 
 const router = express.Router();
@@ -9,7 +10,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/hello', helloController.getHelloWorld);
-
+router.post('/admin/products', productController.addNewProduct);
+router.get('/admin/product', productController.getProductById);
 router.use('/*', (req, res, next) => next(new HttpError(status.NOT_FOUND)));
 router.use(apiErrorHandler);
 
