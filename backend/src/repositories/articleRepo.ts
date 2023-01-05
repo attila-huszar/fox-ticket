44abf632f32bb58168b8445886cec7d1c7d0371f
andrisnemeth <n.andris939@gmail.com>
2023-01-05 17:14:43 +0100

    Add articleRepo.ts file for backend repo.

---
+++ backend/src/repositories/articleRepo.ts
@@ -0,0 +1,11 @@
+import { Article } from '../models/Article';
+import { NewArticleRequest } from '../interfaces/newArticle';
+
+export function getAllArticles(): Promise<Article[]> {
+  return Article.findAll();
+}
+
+export function createArticle(newArticle: NewArticleRequest): Promise<Article> {
+  console.log(newArticle);
+  return Article.create({ ...newArticle });
+}
