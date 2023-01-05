1f70b55928ebb333339f1e9c190dbb0209f72cdf
andrisnemeth <n.andris939@gmail.com>
2023-01-05 17:09:14 +0100

    Add getArticles.ts and newArticle.ts files for backend.

---
+++ backend/src/interfaces/getArticles.ts
@@ -0,0 +1,5 @@
+import { Article } from "../models/Article";
+
+export interface GetAllArticlesResponse {
+  articles: Article[];
+}
---
+++ backend/src/interfaces/newArticle.ts
@@ -0,0 +1,11 @@
+import { Article } from '../models/Article';
+
+export interface NewArticleRequest {
+  title: string;
+  content: string;
+  publish_date: Date;
+}
+
+export interface NewArticleResponse {
+  newArticle: Article;
+}
