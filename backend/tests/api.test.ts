69411b6138975af8c7b785f7521880d4376a0eff
andrisnemeth <n.andris939@gmail.com>
2023-01-05 17:05:53 +0100

    Add test file for /getArticles endpoint.

---
+++ backend/tests/api.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import status from 'http-status';
+import app from '../src/app';
+import { Article } from '../src/models/Article';
+
+describe('GET /api/articles', () => {
+  it('returns all existing articles', async () => {
+    await Article.create({
+      title: 'Cím',
+      content: 'tartalom',
+      publish_date: String(Date.now()),
+    });
+
+    const result = await request(app).get(`/api/articles`);
+
+    expect(result.statusCode).toEqual(status.OK);
+    const { articles } = result.body;
+    expect(articles.length).toEqual(1);
+  });
+});
