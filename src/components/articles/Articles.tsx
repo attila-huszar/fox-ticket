547d00a6f1e81c53ecace0da30d27a8e5cfef047
Attila Huszár <attila.huszar@outlook.com>
2023-01-09 12:32:24 +0100

    Add Articles

---
+++ src/components/articles/Articles.tsx
@@ -0,0 +1,43 @@
+import "./Articles.css";
+import data from "./Data";
+import { Card, Col, Text, Link } from "@nextui-org/react";
+
+export default function ShowArticles() {
+  return (
+    <div className="articles-main">
+      <h2 id="articles-header">Latest News</h2>
+      <div className="articles">
+        {data.articles.map((article) => (
+          <div className="article" key={article.title}>
+            <Card css={{ w: "100%" }}>
+              <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                    color="#9E9E9E"
+                  >
+                    {article.author}
+                  </Text>
+                  <Text h4 color="black">
+                    {article.title}
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Link href={article.url}>
+                <Card.Image
+                  src={article.pic}
+                  width="400px"
+                  height={340}
+                  objectFit="scale-down"
+                  alt="Card image background"
+                />
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
