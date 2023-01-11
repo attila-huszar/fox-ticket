149e7a7b42da41b570b1da431cf396bebaabc422
andrisnemeth <n.andris939@gmail.com>
2023-01-11 11:37:00 +0100

    Add updates to test.

--- backend/tests/order.test.ts
+++ backend/tests/order.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest';
 import status from 'http-status';
-import * as orderRepo from '../src/repositories/orderRepo';
 import app from '../src/app';
 import Order from '../src/models/Order';
 import User from '../src/models/User';
@@ -9,13 +8,11 @@ import Person from '../src/models/person';
 describe('GET /api/purchases/:id', () => {
   it('returns every order with the status for the given id', async () => {
     const person = {
-      name: 'Bácsi'
+      name: 'Bácsi',
     };
-    try{
+
     await Person.create(person);
-    } catch (error){
-      console.log(error)
-    }
+
     const user = {
       name: 'Bácsi',
       email: 'sasa@sadfa.com',
@@ -24,11 +21,8 @@ describe('GET /api/purchases/:id', () => {
       isVerified: true,
       token: 'sdfgdf',
     };
-    try {
-      await User.create(user);
-    } catch (error){
-      console.log(error)
-    }
+
+    await User.create(user);
 
     const order = {
       orderDate: Date.now(),
@@ -38,16 +32,13 @@ describe('GET /api/purchases/:id', () => {
       expirationDate: Date.now(),
       userId: 1,
     };
-    try{
-    await Order.create(order);
-    } catch (error){
-      console.log(error)
-    }
 
-    const result = await request(app).get(`api/purchases/1`);
+    await Order.create(order);
+
+    const result = await request(app).get(`/api/purchases/1`);
 
     expect(result.statusCode).toEqual(status.OK);
-    const { orders } = result.body;
-    expect(orders.length).toEqual(1);
+    const { allOrders } = result.body;
+    expect(allOrders.length).toEqual(1);
   });
 });
