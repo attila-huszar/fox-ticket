92589aacdf726e0fc21fa9b69cf47ba2a46f76c2
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-12 13:01:07 +0100

    update validation error messages

--- backend/src/interfaces/user.ts
+++ backend/src/interfaces/user.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const RegisterUserRequestValidator = z
   .object({
-    name: z.string().min(1, 'Name required'),
+    name: z.string().min(1, 'Name is required'),
     email: z.string().email().min(1, 'Email is required'),
     password: z.string().min(8, 'Password must be at least 8 characters.'),
   })
