c9af9cd0d9abcba148099933f6f7adf146e05d27
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-13 14:28:43 +0100

    remove unnecessary imports

--- backend/src/repositories/articleRepo.ts
+++ backend/src/repositories/articleRepo.ts
@@ -1,9 +1,5 @@
 import Article from '../models/Article';
-import {
-  GetAllArticlesResponse,
-  NewArticleRequest,
-  NewArticleResponse,
-} from '../interfaces/articles';
+import { NewArticleRequest } from '../interfaces/articles';
 
 export function getAllArticles(): Promise<Article[]> {
   return Article.findAll();
