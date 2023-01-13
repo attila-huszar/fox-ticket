f3a6d368eb1b52cd3b45229ee1802709c056592b
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-13 14:24:22 +0100

    add article validation

--- backend/src/services/articleService.ts
+++ backend/src/services/articleService.ts
@@ -3,9 +3,15 @@ import * as articleRepo from '../repositories/articleRepo';
 import {
   GetAllArticlesResponse,
   NewArticleRequest,
+  NewArticleRequestValidator,
   NewArticleResponse,
 } from '../interfaces/articles';
 import { ParameterError } from '../errors';
+import _ from 'lodash';
+
+const articleResponse = (article: object) => {
+  return _.pick(article, ['id', 'title', 'content', 'publish_date']);
+};
 
 export async function getAllArticles(): Promise<GetAllArticlesResponse> {
   const allArticles: Article[] = await articleRepo.getAllArticles();
@@ -18,7 +24,8 @@ export async function addNewArticle(
   if (!newArticle) {
     throw new ParameterError('Invalid article');
   }
+  await NewArticleRequestValidator.parseAsync(newArticle);
   const article = await articleRepo.createArticle(newArticle);
 
-  return { newArticle: article };
+  return articleResponse(article) as NewArticleResponse;
 }
