496036321118881b0553e3745ed5ce976f946b9a
andrisnemeth <n.andris939@gmail.com>
2023-01-17 17:11:59 +0100

    Add modification to shop.tsx and product.ts

--- frontend/src/components/shop/Shop.tsx
+++ frontend/src/components/shop/Shop.tsx
@@ -2,36 +2,83 @@ import { Grid, Button, Container, Card, Row, Text } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 import fetchProducts from '../../api/products';
 import Product from './../../interfaces/product';
+import './shop.css';
+// <---- HA HELYBEN AXIOSSAL FETCH VAN, AKKOR KELL: import axios from 'axios';
 
 export default function Shop() {
   const [products, setProducts] = useState<Product[]>([]);
 
+  // <--------- MŰKŐDŐ FETCH AXIOS-SAL PRODUCT.TS-BŐL ------>
+
   useEffect(() => {
-    fetchProducts().then(productsData => {
-      setProducts(productsData.allProducts);
-    });
+    fetchProducts().then(data => setProducts(data));
   }, []);
+
+  // <--------- MŰKŐDŐ FETCH AXIOS-SAL HELYBEN ------>
+
+  // useEffect(() =>{
+  //   async function fetchData(){
+  //     const request = await axios.get(`/api/products`);
+  //     console.log(request)
+  //     setProducts(request.data.allProducts)
+  //     return request;
+  //   }
+  //   fetchData();
+  // }, [`/api/products`])
+
+  // <--------- MŰKŐDŐ FETCH AXIOS NÉLKÜL ------>
+  // useEffect(() => {
+  //   fetchProducts().then(productsData => {
+  //     setProducts(productsData.allProducts);
+  //   });
+  // }, []);
 
   return (
     <Container id="cardContainer">
-      <Text h1 css={{ marginTop: '20px' }}>
+      <Text h1 css={{ marginTop: '20px', textAlign: 'center' }}>
         Tickets and Passes
       </Text>
+      <Button.Group
+        color="gradient"
+        ghost
+        css={{ width: '100%', justifyContent: 'center' }}
+      >
+        <Button shadow size="md" auto color="gradient" id="ticketsTabButton">
+          Tickets
+        </Button>
+        <Button shadow size="md" auto color="gradient" id="passesTabButton">
+          Passes
+        </Button>
+      </Button.Group>
       <Grid.Container
         id="shopCards"
         gap={2}
-        css={{ display: 'flex', width: '100%' }}
+        css={{
+          // display: 'flex',
+          margin: '0 auto',
+          display: "grid",
+          gridTemplateColumns: "30% 30% 30%",
+          gridTemplateRows: 'repeat(autofill, minmax(300px, 1fr))',
+          columnGap: "5%",
+          width: "80%",
+        }}
       >
-        <Grid sm={12} md={5} id="cardGrid">
-          {products.map(product => (
+        {products.map(product => (
+          <Grid
+            sm={12}
+            md={5}
+            id="cardGrid"
+            css={{ justifyContent: 'flex-start', marginLeft: '10%' }}
+          >
             <Card
               css={{
-                mw: '300px',
+                w: '280px',
                 display: 'flex',
-                minWidth: '250px',
-                margin: '20px 20px 0 20px',
+                margin: '0 auto',
               }}
               id="card"
+              isHoverable
+              isPressable
             >
               <Card.Header>
                 <Text css={{ margin: 'auto' }}>{product.name}</Text>
@@ -50,8 +97,8 @@ export default function Shop() {
                 </Row>
               </Card.Footer>
             </Card>
-          ))}
-        </Grid>
+          </Grid>
+        ))}
       </Grid.Container>
     </Container>
   );
