6ff3f77537f867f6ccf7bf0c86c2e5f55a232eb1
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-18 10:43:25 +0100

    add product interface

---
+++ frontend/src/interfaces/product.ts
@@ -0,0 +1,15 @@
+export interface ProductResponse {
+  id: number;
+  name: string;
+  price: number;
+  duration: number;
+  description: string;
+  type: string;
+}
+
+export interface ProductRequest {
+  name: string;
+  price: number;
+  description: string;
+  isAdmin?: boolean;
+}
