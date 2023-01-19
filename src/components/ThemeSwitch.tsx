5b949fe5653eeb2a5c4251501b5aba074423b137
Attila Huszár <attila.huszar@outlook.com>
2023-01-19 11:48:55 +0100

    Upd

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,36 +1,15 @@
-import { createTheme, useTheme, Switch } from "@nextui-org/react";
+import { changeTheme, Switch, useTheme } from "@nextui-org/react";
 import { TbSun, TbMoon } from "react-icons/tb";
-
-export const lightTheme = createTheme({
-  type: "light",
-  theme: {
-    colors: {
-      background: "#f8fafb",
-      navbarActive: "#cee4fe",
-      navbarLink: "#000",
-      navbarGradient: "linear-gradient(180deg, rgba(248,250,251,1) 0%, rgba(248,250,251,0.8) 30%, rgba(248,250,251,0) 80%)",
-      hoverShadow: "#7450dd",
-    },
-  },
-});
-
-export const darkTheme = createTheme({
-  type: "dark",
-  theme: {
-    colors: {
-      background: "#131a28",
-      navbarActive: "#10253e",
-      navbarLink: "#fff",
-      navbarGradient: "linear-gradient(180deg, rgba(19,26,40,1) 0%, rgba(19,26,40,0.95) 30%, rgba(248,250,251,0) 100%)",
-      hoverShadow: "#7450dd",
-    },
-  },
-});
 
 export default function ThemeSwitch() {
   const { isDark } = useTheme();
 
-  console.log(isDark);
+  const handleChange = () => {
+    const nextTheme = isDark ? "light" : "dark";
+    window.localStorage.setItem("data-theme", nextTheme);
+    changeTheme(nextTheme);
+  };
+
   return (
     <Switch
       css={{
@@ -39,7 +18,7 @@ export default function ThemeSwitch() {
         },
       }}
       checked={isDark}
-      onChange={() => !isDark}
+      onChange={handleChange}
       iconOn={<TbMoon />}
       iconOff={<TbSun />}
       size="lg"
---
+++ src/components/ThemedApp.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { getDocumentTheme, NextUIProvider } from "@nextui-org/react";
+import { createTheme } from "@nextui-org/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const lightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      background: "#f8fafb",
+      navbarActive: "#cee4fe",
+      navbarLink: "#000",
+      navbarGradient: "linear-gradient(180deg, rgba(248,250,251,1) 0%, rgba(248,250,251,0.8) 30%, rgba(248,250,251,0) 80%)",
+      hoverShadow: "#7450dd",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      background: "#131a28",
+      navbarActive: "#10253e",
+      navbarLink: "#fff",
+      navbarGradient: "linear-gradient(180deg, rgba(19,26,40,1) 0%, rgba(19,26,40,0.95) 30%, rgba(248,250,251,0) 100%)",
+      hoverShadow: "#7450dd",
+    },
+  },
+});
+
+export default function Theme() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    let theme = window.localStorage.getItem("data-theme");
+    setIsDark(theme === "dark");
+
+    const observer = new MutationObserver(mutation => {
+      let newTheme = getDocumentTheme(document?.documentElement);
+      setIsDark(newTheme === "dark");
+    });
+
+    observer.observe(document?.documentElement, {
+      attributes: true,
+      attributeFilter: ["data-theme", "style"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <NextUIProvider theme={isDark ? darkTheme : lightTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </NextUIProvider>
+  );
+}
