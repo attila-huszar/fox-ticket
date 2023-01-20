e472b9b65ec1b86b9c8b1992980eafe6060f4cd3
Attila Huszár <attila.huszar@outlook.com>
2023-01-20 02:03:29 +0100

    Upd Login, Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -86,190 +86,201 @@ export default function Profile() {
   }, [passNew, passConf]);
 
   return (
-    <Fade duration={1000}>
-      <Container
-        fluid
-        wrap="wrap"
-        justify="center"
-        style={{
-          margin: "20px auto",
-          minWidth: "450px",
-          maxWidth: "800px",
-          border: "4px solid var(--nextui-colors-navbarActive)",
-          borderRadius: "12px",
-        }}>
-        <Row>
-          <Text
-            size={30}
-            css={{
-              margin: "auto",
-              textGradient: "45deg, $blue600 -20%, $pink600 50%",
-            }}
-            weight="bold">
-            My Profile
-          </Text>
-        </Row>
-        <Spacer y={2} />
-        <Row>
-          <Col>
-            <img
-              src={profile_defpic}
-              style={{
-                borderRadius: "50%",
-                padding: "5px",
-                border: "5px solid var(--nextui-colors-navbarActive)",
-                width: "200px",
-                height: "auto",
+    <>
+      <Fade duration={1000}>
+        <Container
+          style={{
+            margin: "50px auto",
+            padding: "20px",
+            minWidth: "450px",
+            maxWidth: "800px",
+            height: "450px",
+            border: "4px solid var(--nextui-colors-navbarActive)",
+            borderRadius: "12px",
+          }}>
+          <Row>
+            <Text
+              size={30}
+              css={{
                 margin: "auto",
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
               }}
-              alt="profile"
-            />
-            <Row style={{ bottom: "10px" }}>
+              weight="bold">
+              My Profile
+            </Text>
+          </Row>
+          <Spacer y={2} />
+          <Row>
+            <Col style={{ margin: "auto" }}>
+              <img
+                src={profile_defpic}
+                style={{
+                  margin: "0 auto",
+                  borderRadius: "50%",
+                  padding: "5px",
+                  border: "5px solid var(--nextui-colors-navbarActive)",
+                  width: "220px",
+                  aspectRatio: "1",
+                  height: "auto",
+                }}
+                alt="profile"
+              />
+            </Col>
+            <Col>
+              <Input
+                readOnly
+                underlined
+                width="100%"
+                size="lg"
+                labelLeft="Email"
+                value={" admin@foxticket.com"}
+              />
+              <Spacer y={1.5} />
+              <Input
+                underlined
+                width="100%"
+                size="lg"
+                labelLeft="Username"
+                value={" Admin"}
+              />
+            </Col>
+          </Row>
+          <Spacer y={-2} />
+          <Row align="baseline">
+            <Col>
               <Button
-                style={{ fontSize: "1rem", margin: "auto" }}
+                style={{
+                  fontSize: "0.9rem",
+                  margin: "auto",
+                }}
                 auto
                 color="secondary"
                 shadow>
                 Change Picture
               </Button>
-            </Row>
-          </Col>
-          <Col>
-            <Input
-              readOnly
-              underlined
-              width="100%"
-              style={{ margin: "auto" }}
-              labelLeft="Email"
-              value={" admin@foxticket.com"}
-            />
-            <Spacer y={2} />
-            <Input
-              underlined
-              width="100%"
-              style={{ margin: "auto" }}
-              labelLeft="Username"
-              value={" Admin"}
-            />
-            <Spacer y={2} />
-            <Row style={{ bottom: "10px" }}>
+            </Col>
+            <Col>
               <Button
-                style={{ fontSize: "1rem", margin: "auto" }}
+                style={{
+                  fontSize: "0.9rem",
+                  margin: "auto",
+                }}
                 shadow
-                color="gradient"
+                color="primary"
                 id="submit"
                 onPress={userChangeBtnHandler}>
                 Change Username
               </Button>
-              <Spacer y={2} />
+              <Spacer y={1.5} />
               <Button
-                style={{ fontSize: "1rem", margin: "auto" }}
-                color="primary"
+                style={{
+                  fontSize: "0.9rem",
+                  margin: "auto",
+                }}
+                color="gradient"
                 shadow
                 onClick={passChangeBtnHandler}>
                 Change Password
               </Button>
-            </Row>
-          </Col>
-        </Row>
-        <Spacer y={2} />
+            </Col>
+          </Row>
+        </Container>
+      </Fade>
 
-        <Modal
-          closeButton
-          blur
-          aria-labelledby="username change form"
-          open={visChangeUser}
-          onClose={closeHandler}>
-          <Modal.Header>
-            <Text size={18}>Change your Username</Text>
-          </Modal.Header>
-          <Modal.Body>
-            <Spacer y={0.4} />
-            <Input
-              onChange={e => setPassOld(e.target.value)}
-              labelPlaceholder="New Username"
-              width="350px"
-              required
-              bordered
-              size="lg"
-            />
+      <Modal
+        closeButton
+        blur
+        aria-labelledby="username change form"
+        open={visChangeUser}
+        onClose={closeHandler}>
+        <Modal.Header>
+          <Text size={18}>Change your Username</Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Spacer y={0.4} />
+          <Input
+            onChange={e => setPassOld(e.target.value)}
+            labelPlaceholder="New Username"
+            width="350px"
+            required
+            bordered
+            size="lg"
+          />
 
-            <Spacer y={0.4} />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button auto flat color="error" onPress={closeHandler}>
-              Close
-            </Button>
-            <Button auto color="gradient" onPress={closeHandler}>
-              Change
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
-        <Modal
-          closeButton
-          blur
-          aria-labelledby="password change form"
-          open={visChangePass}
-          onClose={closeHandler}>
-          <Modal.Header>
-            <Text size={18}>Change your Password</Text>
-          </Modal.Header>
-          <Modal.Body>
-            <Spacer y={0.4} />
-            <Input.Password
-              onChange={e => setPassOld(e.target.value)}
-              labelPlaceholder="Current Password"
-              width="350px"
-              required
-              bordered
-              status={passOldHelper.color}
-              color={passOldHelper.color}
-              helperColor={passOldHelper.color}
-              helperText={passOldHelper.text}
-              type="password"
-              size="lg"
-            />
-            <Spacer y={1.6} />
-            <Input.Password
-              onChange={e => setPassNew(e.target.value)}
-              labelPlaceholder="New Password"
-              width="350px"
-              required
-              bordered
-              status={passNewHelper.color}
-              color={passNewHelper.color}
-              helperColor={passNewHelper.color}
-              helperText={passNewHelper.text}
-              type="password"
-              size="lg"
-            />
-            <Spacer y={1.6} />
-            <Input.Password
-              onChange={e => setPassConf(e.target.value)}
-              labelPlaceholder="Confirm Password"
-              width="350px"
-              required
-              bordered
-              status={passConfHelper.color}
-              color={passConfHelper.color}
-              helperColor={passConfHelper.color}
-              helperText={passConfHelper.text}
-              type="password"
-              size="lg"
-            />
-            <Spacer y={0.4} />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button auto flat color="error" onPress={closeHandler}>
-              Close
-            </Button>
-            <Button auto color="gradient" onPress={closeHandler}>
-              Change
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Container>
-    </Fade>
+          <Spacer y={0.4} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={closeHandler}>
+            Close
+          </Button>
+          <Button auto color="gradient" onPress={closeHandler}>
+            Change
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        closeButton
+        blur
+        aria-labelledby="password change form"
+        open={visChangePass}
+        onClose={closeHandler}>
+        <Modal.Header>
+          <Text size={18}>Change your Password</Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Spacer y={0.4} />
+          <Input.Password
+            onChange={e => setPassOld(e.target.value)}
+            labelPlaceholder="Current Password"
+            width="350px"
+            required
+            bordered
+            status={passOldHelper.color}
+            color={passOldHelper.color}
+            helperColor={passOldHelper.color}
+            helperText={passOldHelper.text}
+            type="password"
+            size="lg"
+          />
+          <Spacer y={1.6} />
+          <Input.Password
+            onChange={e => setPassNew(e.target.value)}
+            labelPlaceholder="New Password"
+            width="350px"
+            required
+            bordered
+            status={passNewHelper.color}
+            color={passNewHelper.color}
+            helperColor={passNewHelper.color}
+            helperText={passNewHelper.text}
+            type="password"
+            size="lg"
+          />
+          <Spacer y={1.6} />
+          <Input.Password
+            onChange={e => setPassConf(e.target.value)}
+            labelPlaceholder="Confirm Password"
+            width="350px"
+            required
+            bordered
+            status={passConfHelper.color}
+            color={passConfHelper.color}
+            helperColor={passConfHelper.color}
+            helperText={passConfHelper.text}
+            type="password"
+            size="lg"
+          />
+          <Spacer y={0.4} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={closeHandler}>
+            Close
+          </Button>
+          <Button auto color="gradient" onPress={closeHandler}>
+            Change
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
