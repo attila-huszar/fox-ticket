4ad8d5194e055fc83acccbc6fc375a035fbc3327
Attila Huszár <attila.huszar@outlook.com>
2023-01-22 16:32:53 +0100

    Upd Email Verification, App, dependencies

--- backend/src/services/sendVerificationEmail.ts
+++ backend/src/services/sendVerificationEmail.ts
@@ -21,7 +21,8 @@ export async function sendVerificationEmail(): Promise<string> {
     from: "attila.huszar@outlook.com",
     subject: "Email verification - Fox Ticket",
     text: `Please verify your email by copying this link to the browser window: ${link}`,
-    html: `<h2 style="text-align: center;">Please verify your email by clicking <strong><a href=${link}>this link</a></strong></h2><p style="text-align: center;"><em>Link is valid for 24 hours<em></p>`,
+    html: `<div style="font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; text-align: center;"<h1 style="padding: 20px;"><img src="https://ams02pap001files.storage.live.com/y4msjTbBRoUnECddDsR_L75gdxST96UH9pxH5az2WXLSTaO5AtY4mtpT0ZsP0r-BK8ESVM9DMhvcdPEj7R99jlgPGEYouL7ezI03ofy10ZyIZLUtnYJhBsR_SzCoLDhPZKZEyJ_B5ioab6QbxbHcal1WMzo6lr-_k8PzFvn_uWtGvzuIdk6j4f1KsE5GDi8xhOK?width=256&height=255&cropmode=none" width="56" height="55" /><b>Fox</b>Ticket</h1>
+    <h2>Please verify your email by clicking <b><a href=${link}>this link</a></b>.</h2><p><i>Link is valid for 24 hours.<i></p>`,
   };
 
   sgMail
