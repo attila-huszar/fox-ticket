937b4b7245a31ed41e0f8a6a54fb4bed8ad8e87b
Gergely Valkai <gergely.valkai03@gmail.com>
2023-01-24 12:29:02 +0100

    update component functions

--- frontend/src/components/AddProduct.tsx
+++ frontend/src/components/AddProduct.tsx
@@ -1,108 +1,164 @@
 import {
   Text,
-  Container,
   Spacer,
   Button,
   Row,
   Col,
   Input,
+  Modal,
 } from '@nextui-org/react';
 import { useState } from 'react';
+import { fetchAddNewProduct } from '../api/product';
+import { ProductRequest, ProductResponse } from '../interfaces/product';
 
-export function AddProduct() {
-  const [productName, setProductName] = useState('');
-  const [price, setPrice] = useState('');
-  const [duration, setDuration] = useState('');
-  const [description, setDescription] = useState('');
-  const [type, setType] = useState('');
+export function AddProduct({ addProduct }: { addProduct: (newProduct: ProductResponse) => void }) {
+  const emptyproduct = {
+    name: '',
+    price: 0,
+    duration: 0,
+    description: '',
+    type: '',
+  };
+  const [data, setData] = useState<ProductRequest>(emptyproduct);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [visAddProduct, setVisAddProduct] = useState(false);
 
+  const addProductHandler = async () => {
+    try {
+      addProduct(await fetchAddNewProduct(data));
+    } catch (error) {
+      if (error instanceof Error) {
+        const errors = [];
+        errors.push(error.message.split(';'));
 
-  const addProductHandler = () => {
+        for (let i = 0; i < errors.length; i++) {
+          setErrorMessage(errors[0][i]);
+        }
+        return;
+      }
+    }
+    setErrorMessage('');
+    setData(emptyproduct);
+  };
 
+  const productButtonHandler = () => setVisAddProduct(true);
+
+  const closeHandler = () => {
+    setVisAddProduct(false);
   };
 
   return (
-    <Container
-      fluid
-      wrap="wrap"
-      justify="center"
-      style={{
-        margin: '20px auto',
-        minWidth: '450px',
-        maxWidth: '500px',
-        border: '4px solid var(--nextui-colors-navbarActive)',
-        borderRadius: '12px',
-      }}
-    >
-      <Row>
-        <Text
-          size={30}
-          css={{
-            margin: 'auto',
-            textGradient: '45deg, $blue600 -20%, $pink600 50%',
-          }}
-          weight="bold"
-        >
-          Add New Product
-        </Text>
-      </Row>
-      <Spacer y={0.5} />
-      <Row>
-        <Col>
-          <Input
-            onChange={e => setProductName(e.target.value)}
-            underlined
-            width="100%"
-            style={{ margin: 'auto' }}
-            labelLeft="Name"
-          />
-          <Spacer y={0.5} />
-          <Input
-            onChange={e => setPrice(e.target.value)}
-            underlined
-            width="100%"
-            style={{ margin: 'auto' }}
-            labelLeft="Price"
-          />
-          <Spacer y={0.5} />
-          <Input
-            onChange={e => setDuration(e.target.value)}
-            underlined
-            width="100%"
-            style={{ margin: 'auto' }}
-            labelLeft="Duration"
-          />
-          <Spacer y={0.5} />
-          <Input
-            onChange={e => setDescription(e.target.value)}
-            underlined
-            width="100%"
-            style={{ margin: 'auto' }}
-            labelLeft="Description"
-          />
-          <Spacer y={0.5} />
-
-          <Input
-            onChange={e => setType(e.target.value)}
-            underlined
-            width="100%"
-            style={{ margin: 'auto' }}
-            labelLeft="Type"
-          />
-          <Spacer y={1} />
+    <>
+      <Button
+        style={{ fontSize: '1rem', margin: 'auto' }}
+        auto
+        color="secondary"
+        shadow
+        onClick={productButtonHandler}
+      >
+        Add Product
+      </Button>
+      <Modal
+        closeButton
+        blur
+        aria-labelledby="login form"
+        open={visAddProduct}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
           <Row>
-            <Button
-              onPress={addProductHandler}
-              style={{ fontSize: '1rem', margin: 'auto', marginBottom: '30px' }}
-              shadow
-              color="gradient"
-              id="submit"
+            <Text
+              size={30}
+              css={{
+                margin: 'auto',
+                textGradient: '45deg, $blue600 -20%, $pink600 50%',
+              }}
+              weight="bold"
             >
-              Add Product
-            </Button>
+              Add New Product
+            </Text>
           </Row>
-        </Col>
-      </Row>
-    </Container>
+        </Modal.Header>
+        <Modal.Body>
+          <Spacer y={0.5} />
+          <Row>
+            <Col>
+              <Input
+                onChange={e => setData({ ...data, name: e.target.value })}
+                underlined
+                width="100%"
+                style={{ margin: 'auto' }}
+                labelLeft="Name"
+                value={data.name}
+              />
+              <Spacer y={0.5} />
+              <Input
+                onChange={e =>
+                  setData({ ...data, price: Number(e.target.value) })
+                }
+                underlined
+                width="100%"
+                style={{ margin: 'auto' }}
+                labelLeft="Price"
+                value={data.price === 0 ? '' : data.price}
+              />
+              <Spacer y={0.5} />
+              <Input
+                onChange={e =>
+                  setData({ ...data, duration: Number(e.target.value) })
+                }
+                underlined
+                width="100%"
+                style={{ margin: 'auto' }}
+                labelLeft="Duration"
+                value={data.duration === 0 ? '' : data.duration}
+              />
+              <Spacer y={0.5} />
+              <Input
+                onChange={e =>
+                  setData({ ...data, description: e.target.value })
+                }
+                underlined
+                width="100%"
+                style={{ margin: 'auto' }}
+                labelLeft="Description"
+                value={data.description}
+              />
+              <Spacer y={0.5} />
+              <Input
+                onChange={e => setData({ ...data, type: e.target.value })}
+                underlined
+                width="100%"
+                style={{ margin: 'auto' }}
+                labelLeft="Type"
+                value={data.type}
+              />
+              <Spacer y={1} />
+              <Text css={{ textAlign: 'center' }} color="error">
+                {errorMessage}
+              </Text>
+              <Row>
+                <Button
+                  onPress={addProductHandler}
+                  style={{
+                    fontSize: '1rem',
+                    margin: 'auto',
+                    marginBottom: '30px',
+                  }}
+                  shadow
+                  color="gradient"
+                  id="submit"
+                >
+                  Add Product
+                </Button>
+                <Button auto flat color="error" onPress={closeHandler}>
+                  Close
+                </Button>
+              </Row>
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
