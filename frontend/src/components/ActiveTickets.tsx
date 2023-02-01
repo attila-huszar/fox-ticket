9560355518b09c14d2d55175781a225ce77ecec7
Gergely Valkai <gergely.valkai03@gmail.com>
2023-02-01 12:11:32 +0100

    create ActiveTickets component

---
+++ frontend/src/components/ActiveTickets.tsx
@@ -0,0 +1,53 @@
+import { Grid, Button, Card, Row, Text } from '@nextui-org/react';
+import { ProductRequest } from '../interfaces/product';
+
+export default function ActiveTickets({
+  id,
+  name,
+  description,
+}: ProductRequest) {
+  return (
+    <Grid
+      sm={12}
+      md={5}
+      id="cardGrid"
+      css={{
+        justifyContent: 'center',
+        marginLeft: '32%',
+        width: '100%',
+        padding: '0',
+      }}
+    >
+      <Card
+        css={{
+          w: '280px',
+          display: 'flex',
+          margin: '0 auto',
+          backgroundColor: 'var(--nextui-colors-cardBackground)',
+        }}
+        id="card"
+        isHoverable
+      >
+        <Card.Header
+          css={{ backgroundColor: 'var(--nextui-colors-cardHeaderBackground)' }}
+        >
+          <Text css={{ color: 'White', margin: 'auto', fontSize: 'larger' }}>
+            {name}
+          </Text>
+        </Card.Header>
+        <Card.Divider />
+        <Card.Body css={{ py: '$10' }}>
+          <Text css={{ margin: 'auto' }}>{description}</Text>
+        </Card.Body>
+        <Card.Divider />
+        <Card.Footer>
+          <Row justify="center">
+              <Button shadow size="md" auto color="gradient" id="submit">
+                Activate
+              </Button>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Grid>
+  );
+}
