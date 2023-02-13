7ed8afdaed90811d4048784e6397a90f00ed0700
Attila Huszár <attila.huszar@outlook.com>
2023-02-13 19:14:13 +0100

    Upd register.test with mock

--- backend/tests/register.test.ts
+++ backend/tests/register.test.ts
@@ -3,27 +3,29 @@ import status from 'http-status';
 import * as userRepo from '../src/repositories/userRepo';
 import app from '../src/app';
 
+jest.mock('../src/services/sendVerificationEmail.ts');
+
 describe('POST /api/register', () => {
-//   it('returns the JSON for the registered user', async () => {
-//     const newUser = {
-//       name: 'John Doe',
-//       email: 'asd@asd.com',
-//       password: 'johndoe123',
-//     };
+  it('returns the JSON for the registered user', async () => {
+    const newUser = {
+      name: 'John Doe',
+      email: 'asd@asd.com',
+      password: 'johndoe123',
+    };
 
-//     const result = await request(app).post(`/api/register`).send(newUser);
+    const result = await request(app).post(`/api/register`).send(newUser);
 
-//     expect(result.statusCode).toEqual(status.OK);
-//     const user = result.body;
-//     expect(user).toEqual({
-//       id: 1,
-//       name: 'John Doe',
-//       email: 'asd@asd.com',
-//       isAdmin: false,
-//       isVerified: false,
-//     });
-//     expect(userRepo.getUserByName('John Doe')).toBeTruthy();
-//   });
+    expect(result.statusCode).toEqual(status.OK);
+    const user = result.body;
+    expect(user).toEqual({
+      id: 1,
+      name: 'John Doe',
+      email: 'asd@asd.com',
+      isAdmin: false,
+      isVerified: false,
+    });
+    expect(userRepo.getUserByName('John Doe')).toBeTruthy();
+  });
 
   it('returns Bad Request for missing parameter', async () => {
     const newUser = {
@@ -52,25 +54,26 @@ describe('POST /api/register', () => {
     });
   });
 
-  //   it('should send an error back with a message of Email already taken', async () => {
-  //     const newUser = {
-  //       name: 'John Wick',
-  //       email: 'asd@asd.com',
-  //       password: 'johnwick123',
-  //     };
-  //     const person = await userRepo.registerUser({
-  //       name: 'John Doe',
-  //       email: 'asd@asd.com',
-  //       password: 'johndoe123',
-  //     });
+  it('should send an error back with a message of Email already taken', async () => {
+    const newUser = {
+      name: 'John Wick',
+      email: 'asd@asd.com',
+      password: 'johnwick123',
+    };
+    const person = await userRepo.registerUser({
+      name: 'John Doe',
+      email: 'asd@asd.com',
+      password: 'johndoe123',
+      verificationToken: '',
+    });
 
-  //     const result = await request(app).post(`/api/register`).send(newUser);
+    const result = await request(app).post(`/api/register`).send(newUser);
 
-  //     expect(result.statusCode).toEqual(status.BAD_REQUEST);
-  //     const user = result.body;
-  //     expect(user).toEqual({
-  //       message: 'Validation error: Account already exists',
-  //     });
-  //     expect(userRepo.getUserByEmail('asd@asd.com')).toBeTruthy();
-  //   });
+    expect(result.statusCode).toEqual(status.BAD_REQUEST);
+    const user = result.body;
+    expect(user).toEqual({
+      message: 'Validation error: Account already exists',
+    });
+    expect(userRepo.getUserByEmail('asd@asd.com')).toBeTruthy();
+  });
 });
