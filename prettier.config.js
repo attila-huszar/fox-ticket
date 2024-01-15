a05da348182fd2993512d28a0142ffbb66195121
Attila Huszár <attila.huszar@outlook.com>
2024-01-15 22:05:20 +0100

    Bump pkgs

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
